1d4286d11f5c1006ebfcc8c32c541c3e0679e3ec
AlexSanLo <san.lope.alex@gmail.com>
2025-02-24 12:45:16 +0100

    Cards_Info+Responsivo

--- app/components/Cards/Persona.tsx
+++ app/components/Cards/Persona.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function UserPersona() {
+export default function Persona() {
   return (
     <div className="items-start bg-card-bg-light dark:bg-card-bg-dark rounded-lg p-4">
       <div className="flex flex-col items-center bg-card-bg-light dark:bg-card-bg-dark">
@@ -10,32 +10,32 @@ export default function UserPersona() {
           className="rounded-lg object-cover w-full h-auto mb-4"
         />
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">NAME:</span>
           <h1 className="text-left flex-1">35</h1>
         </div>
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">AGE:</span>
           <h1 className="text-left flex-1">Married, 2 kids</h1>
         </div>
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">EDUCATION:</span>
           <h1 className="text-left flex-1">University of Calgary</h1>
         </div>
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">LOCATION:</span>
           <h1 className="text-left flex-1">Calgary, Alberta</h1>
         </div>
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">JOB:</span>
           <h1 className="text-left flex-1">Family Physician</h1>
         </div>
 
-        <div className="text-lg w-full flex justify-between mb-1 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
+        <div className="text-lg w-full flex justify-between mb-1.5 px-4 font-bold text-card-h2-light dark:text-card-h2-dark">
           <span className="w-50 text-left">HOBBIES:</span>
           <h1 className="text-left flex-1">Hiking and dancing</h1>
         </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import ThemeToggle from "~/components/ThemeToggle/ThemeToggle";
 import Persona from "~/components/Cards/Persona";
+import InfoPersona from "~/components/Cards/InfoPersona";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -19,25 +20,25 @@ export default function Home() {
             User Persona
           </h2>
           <hr className="bg-primary-light dark:bg-primary-dark py-1 border-0 rounded-3xl w-40"/>
-
         </div>
 
-        <div className="col-span-1 col-start-12 py-3">
+        <div className="col-span-1 col-start-12 py-3 ms-auto">
           <ThemeToggle />
         </div>
       </div>
     </section>
 
-    <section className="container mx-auto">
-      <div className="grid grid-cols-12 gap-4 me-20 ms-20">
-        <div className="col-span-4 col-start-1">
+    <section className="container mx-auto pb-6">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4 me-20 ms-20">
+        <div className="col-span-12 md:col-span-4 col-start-1">
           <Persona />
         </div>
         
-        <div className="col-start-5 col-span-8">
-          <div>
-
-          </div>
+        <div className="col-span-12 md:col-start-5 md:col-span-8 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <InfoPersona titulo="Bio" contenido="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies aliquam. Nullam nec purus nec libero ultricies aliquam." />
+          <InfoPersona titulo="Personality" contenido="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies aliquam. Nullam nec purus nec libero ultricies aliquam." />
+          <InfoPersona titulo="Pain points" contenido="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies aliquam. Nullam nec purus nec libero ultricies aliquam." />
+          <InfoPersona titulo="Goals" contenido="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies aliquam. Nullam nec purus nec libero ultricies aliquam." />
         </div>
       </div>
     </section>
